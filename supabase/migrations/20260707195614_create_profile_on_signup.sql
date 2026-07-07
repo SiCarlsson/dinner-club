5d792017a098776751b1a7c999f20bff567d994b
Simon Carlsson <operator_ax.09@icloud.com>
2026-07-07 22:49:34 +0200

    Add Supabase trigger to Pofile table

---
+++ supabase/migrations/20260707195614_create_profile_on_signup.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+RETURNS trigger AS $$
+BEGIN
+  INSERT INTO public.profiles (id, email)
+  VALUES (new.id, new.email);
+  RETURN new;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+CREATE OR REPLACE TRIGGER on_auth_user_created
+  AFTER INSERT ON auth.users
+  FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
